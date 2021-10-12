23a881ba5741614ee2d9d9c4af0a1cee19e4c204
thinh19981998 <dpthinh1508@gmail.com>
2021-10-12 13:23:43 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './sharecomponents/Button';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='center'>
+      <h1>Buttons</h1>
+      {'<Button/>'}
+      <br />
+      <br />
+      <Button />
+      <br />
+      <br />
+      {"<Button variant = 'outline'/>"}
+      <br />
+      <br />
+      <Button variant='outline' />
+      <br />
+      <br />
+      {"<Button variant = 'text'/>"}
+      <br />
+      <br />
+      <Button variant='text' />
+      <br />
+      <br />
+      {'<Button disableShadow/>'}
+      <br />
+      <br />
+      <Button disableShadow />
+      <br />
+      <br />
+      {'<Button disabled/>'}
+      <br />
+      <br />
+      <Button disabled />
+      <br />
+      <br />
+      {'<Button variant="text" disabled/>'}
+      <br />
+      <br />
+      <Button variant='text' disabled />
+      <br />
+      <br />
+      {"<Button startIcon='local_grocery_store'/>"}
+      <br />
+      <br />
+      <Button startIcon='local_grocery_store' />
+      <br />
+      <br />
+      <br />
+      <br />
+      {"<Button endIcon='local_grocery_store'/>"}
+      <br />
+      <br />
+      <Button endIcon='local_grocery_store' />
+      <br />
+      <br />
+      {"<Button size='sm'/>"}
+      <br />
+      <br />
+      <Button size='sm' color='primary' />
+      <br />
+      <br />
+      {"<Button size='md'/>"}
+      <br />
+      <br />
+      <Button size='md' color='primary' />
+      <br />
+      <br />
+      {"<Button size='lg'/>"}
+      <br />
+      <br />
+      <Button size='lg' color='primary' />
+      <br />
+      <br />
+      {"<Button color='default'/>"}
+      <br />
+      <br />
+      <Button color='default' />
+      <br />
+      <br />
+      {"<Button color='primary'/>"}
+      <br />
+      <br />
+      <Button color='primary' />
+      <br />
+      <br />
+      {"<Button color='secondary'/>"}
+      <br />
+      <br />
+      <Button color='secondary' />
+      <br />
+      <br />
+      {"<Button color='danger'/>"}
+      <br />
+      <br />
+      <Button color='danger' />
     </div>
   );
 }
